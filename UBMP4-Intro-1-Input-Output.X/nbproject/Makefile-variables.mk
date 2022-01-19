2b536db5cc4c75d5bbf0c41340eb2c522f1cf439
Sam Warren <90916380+Sinnamonroll413@users.noreply.github.com>
2022-01-19 09:58:53 -0500

    Initial commit

---
+++ UBMP4-Intro-1-Input-Output.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=UBMP4-Intro-1-Input-Output.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/UBMP4-Intro-1-Input-Output.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=ubmp4-intro-1-input-output.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ubmp4-intro-1-input-output.x.tar
+# pro configuration
+CND_ARTIFACT_DIR_pro=dist/pro/production
+CND_ARTIFACT_NAME_pro=UBMP4-Intro-1-Input-Output.X.production.hex
+CND_ARTIFACT_PATH_pro=dist/pro/production/UBMP4-Intro-1-Input-Output.X.production.hex
+CND_PACKAGE_DIR_pro=${CND_DISTDIR}/pro/package
+CND_PACKAGE_NAME_pro=ubmp4-intro-1-input-output.x.tar
+CND_PACKAGE_PATH_pro=${CND_DISTDIR}/pro/package/ubmp4-intro-1-input-output.x.tar
